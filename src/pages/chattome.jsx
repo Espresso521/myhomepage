cf8df574ad5cef8cbcbc9c84bef571027ae02066
Espresso521 <kotaku20220424@gmail.com>
2022-10-17 20:10:51 +0900

    forbidden auto scale

--- src/pages/chattome.jsx
+++ src/pages/chattome.jsx
@@ -1,7 +1,7 @@
 import '../index.css'
 import avatar from '../images/gatsby-icon.png'
 import { v4 as uuid } from 'uuid'
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import ChatInput from '../components/chatinput'
 import { PageHeader, Descriptions, Avatar } from 'antd'
 import ScrollToBottom from 'react-scroll-to-bottom'
@@ -28,6 +28,19 @@ function Chat () {
 
   const [data, setDate] = useState(state)
 
+  useEffect(() => {
+    // 添加禁止缩放的meta标签
+    // <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0;"/>
+    Array.from(document.getElementsByTagName('meta')).filter(i => {
+      if (i.name === 'viewport') {
+        //不允许缩放
+        // <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no,minimum-scale=1.0,maximum-scale=1.0,user-scalable=no">
+        i.content = "width=device-width, initial-scale=1, shrink-to-fit=no,minimum-scale=1.0,maximum-scale=1.0,user-scalable=no"
+      }
+      return i
+    })
+  }, [])
+
   const submitComment = (comment) => {
     console.log("comment is " + comment)
     if (comment === null || comment === '') return
@@ -52,8 +65,8 @@ function Chat () {
         <PageHeader
           className="site-page-header"
           onBack={() => window.location.href = "/"}
-          title="Just do IT"
-          subTitle="Welcome to this chat room"
+          title="Do IT"
+          subTitle="Server By GO"
         />
 
         {/* 评论列表 */}
